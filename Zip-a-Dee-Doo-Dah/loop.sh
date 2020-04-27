cc77a41322d6e3a9b1529bb845b196a56a30e99f
page2me kitarotao <page2me@msn.com>
2020-04-27 11:06:18 +0700

    Add files via upload

---
+++ Zip-a-Dee-Doo-Dah/loop.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+x=1819
+while [ $x -gt -1  ]
+do
+  typ=`ls $x.*|cut -f2 -d.`
+  #echo $x.$typ
+  case $typ in
+    gz)
+      out=`gzip -fkNd $x.$typ`
+      ;;
+    tar)
+      out=`tar -kxvf $x.$typ`
+      ;;
+    zip)
+      pas=`fcrackzip -Du $x.$typ -p rockyou.txt|grep "=="|cut -f3 -d= |tr -d ' \n'`
+      echo $x.$typ:$pas
+      out=`unzip -oP $pas $x.$typ`
+      ;;
+  esac
+  x=$(( $x - 1 ))
+done
+#out=`cat flag.txt`

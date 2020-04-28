1a7b48d7a35d48aad215718bfb00e974f9a47aa6
page2me kitarotao <page2me@msn.com>
2020-04-28 11:41:22 +0700

    Update pass4dec.py

--- thedanzman/pass4dec.py
+++ thedanzman/pass4dec.py
@@ -1,16 +1,17 @@
 import base64
 import codecs
 
+def wow(x):
+  return x[::-1]
+  
 base64_ciphertext = '=ZkXipjPiLIXRpIYTpQHpjSQkxIIFbQCK1FR3DuJZxtPAtkR'
 key = 'nyameowpurrpurrnyanyapurrpurrnyanya'
 key = codecs.decode(key, "rot_13")
+ciphertext = wow(base64_ciphertext)
+ciphertext = codecs.decode(ciphertext, "rot_13")
 
 def bxor(ba1, ba2):
     return bytes([_a ^ _b for _a, _b in zip(ba1, ba2)])
 
-def wow(x):
-  return x[::-1]
-ciphertext = wow(base64_ciphertext)
-ciphertext = codecs.decode(ciphertext, "rot_13")
 flag = bxor(base64.b64decode(ciphertext), key.encode())
 print(flag)

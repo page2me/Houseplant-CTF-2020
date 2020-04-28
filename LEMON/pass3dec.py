f31a5f51f89efe4178805a4639dcf6eef953e2ce
page2me kitarotao <page2me@msn.com>
2020-04-28 10:49:51 +0700

    Add files via upload

---
+++ LEMON/pass3dec.py
@@ -0,0 +1,12 @@
+import base64
+
+#base64_ciphertext = 'X10iNHk5fQ4HIGBxPnwPU3c='
+base64_ciphertext = 'HxEMBxUAURg6I0QILT4UVRolMQFRHzokRBcmAygNXhkqWBw='
+key = 'meownyameownyameownyameownyameownya'
+
+def bxor(ba1, ba2):
+    """ XOR two byte strings """
+    return bytes([_a ^ _b for _a, _b in zip(ba1, ba2)])
+
+flag = bxor(base64.b64decode(base64_ciphertext), key.encode())
+print(flag)

